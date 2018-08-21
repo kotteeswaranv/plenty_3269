e598eb510b59d2e210d4130884c281f4d3807256
kotteeswaranv <kotteeswaran.visu@gmail.com>
2018-08-21 19:57:12 +0530

    Update NovalnetConstants.php

--- src/Constants/NovalnetConstants.php
+++ src/Constants/NovalnetConstants.php
@@ -8,7 +8,7 @@ namespace Novalnet\Constants;
  */
 class NovalnetConstants
 {
-    const PLUGIN_VERSION = '7.0.0-NN(1.0.2)';
+    const PLUGIN_VERSION = '7.0.0-NN(1.0.3)';
     const PAYPORT_URI    = 'https://payport.novalnet.de/paygate.jsp';
     const PAYGATE_URI    = 'https://paygate.novalnet.de/paygate.jsp';
 }
